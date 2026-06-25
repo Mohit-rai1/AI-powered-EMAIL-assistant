69e79f1ca88ed3fc3b90c092fa51e0721038df7b
Mohit-rai1 <2005rai.mohit@gmail.com>
2026-06-25 23:03:41 +0530

    Initial commit

---
+++ email-assistant-backend/email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -0,0 +1,89 @@
+package com.email.writer.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.Map;
+
+
+@Service    //helps us calling gemini api call
+public class EmailGeneratorService {
+
+    private final WebClient webClient;
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+    public EmailGeneratorService(WebClient.Builder webClientBuilder)
+    {
+        this.webClient=webClientBuilder.build();//just creating object of webclient
+
+    }
+
+    public String generateEmailReply(EmailRequest emailRequest)
+    {
+        //prompting here
+         String prompt =buildPrompt(emailRequest);
+        //craft a request
+        Map<String,Object> requestBody;
+        requestBody = Map.of("contents",new Object[]{
+                        Map.of("parts",new Object[]{
+                                Map.of("text",prompt)
+                        })
+    }
+        );
+
+        //do request and get response
+        String response=webClient.post()
+                .uri(geminiApiUrl + geminiApiKey)
+                .header("Content-Type","application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        //{either you can hardcode url and api key but we should not do it}
+        //extract and return  response
+        return extractResponseContent(response);
+
+    }
+
+    private String extractResponseContent(String response) {
+
+        try{
+            ObjectMapper mapper=new ObjectMapper();         //helps handling josn and java object
+            JsonNode rootNode=mapper.readTree(response);   //converts json to tree like structure
+            return rootNode.path("candidates")
+                    .get(0)                               //getting first element of array candidates
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+
+
+        }
+        catch(Exception e){
+            return "Error processing request: "+e.getMessage();
+
+        }
+    }
+
+    private String buildPrompt(EmailRequest emailRequest)
+    {
+        StringBuilder prompt=new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email.please dont use the subject line ");
+        if(emailRequest.getTone()!=null && !emailRequest.getTone().isEmpty())
+        {
+            prompt.append("Use a ").append(emailRequest.getTone()).append("tone ");
+
+        }
+        prompt.append(" \n Original email : \n ").append(emailRequest.getEmailContent());
+        return prompt.toString();
+
+    }
+}
